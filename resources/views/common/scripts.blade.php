c1decc76a4e8a7fe59d558e1d01171558042f7f7
Michael Dahlke <mdahlke@wisnet.com>
2019-12-01 21:29:54 -0600

    fix path to manifest.js

--- resources/views/common/scripts.blade.php
+++ resources/views/common/scripts.blade.php
@@ -1,4 +1,4 @@
-<script src="{{ mix('manifest.js') }}"></script>
+<script src="{{ mix('js/manifest.js') }}"></script>
 <script src="{{ mix('vendor.js') }}"></script>
 <script src="{{ mix('js/app.js') }}"></script>
 @stack('footer')
