d9b5f57f578c681bc5f1f8f4cc3857580b5d4328
Michael Dahlke <mdahlke@wisnet.com>
2019-12-13 15:08:05 -0600

    set no interaction on artisan commands

--- deploy/production-server-commands.sh
+++ deploy/production-server-commands.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 deploy_dir=$1
-test_branch=$2
+branch=$2
 deployUser=$3
 user=$4
 destination=$5
 
 echo "Extracting files"
-unzip -oq ${deploy_dir}${test_branch}-release.zip -d ${deploy_dir}${test_branch}-release
+unzip -oq ${deploy_dir}${branch}-release.zip -d ${deploy_dir}${branch}-release
 
 echo "Fixing permissions"
-chown -R $deployUser:$user $destination ${deploy_dir}${test_branch}-release
+chown -R $deployUser:$user $destination ${deploy_dir}${branch}-release
 
 chgrp -R www-data ${destination}storage ${destination}bootstrap/cache
 chmod -R ug+rwx ${destination}storage ${destination}bootstrap/cache
 
 echo "Copying files to deployment directory (${deploy_dir})"
-rsync -a ${deploy_dir}${test_branch}-release/. ${destination}/ --exclude ${deploy_dir}${test_branch}-release/node_modules/.cache
+rsync -a ${deploy_dir}${branch}-release/. ${destination}/ --exclude ${deploy_dir}${branch}-release/node_modules/.cache
 
 echo "Removing unneeded files from production"
 rm -rf ${destination}/deploy ${destination}/.env.example ${destination}/.git* ${destination}/package* ${destination}/phpunit.xml ${destination}/webpack.mix.js
 
 echo "Cleaning up"
-rm -rf ${deploy_dir}${test_branch}-release.zip ${deploy_dir}${test_branch}-release
+rm -rf ${deploy_dir}${branch}-release.zip ${deploy_dir}${branch}-release
 
-cd /home/${deployUser}/app/ && php artisan config:clear && php artisan cache:clear && php artisan migrate
+cd /home/${deployUser}/app/ && \
+ php artisan config:clear --no-interaction && \
+ php artisan cache:clear --no-interaction && \
+ php artisan config:cache --no-interaction && \
+ php artisan route:cache --no-interaction && \
+ php artisan migrate --no-interaction
 
 #echo "Restarting Lsyncd to ensure files transfer"
 #sudo service lsyncd restart & > /dev/null 2>&1
