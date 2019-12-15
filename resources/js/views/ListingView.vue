6438d47cfe0a89317ed8cacc4deb51fa8447d9b5
Michael Dahlke <mdahlke@wisnet.com>
2019-12-15 15:04:01 -0600

    close listing goes to `listings` vue route

--- resources/js/views/ListingView.vue
+++ resources/js/views/ListingView.vue
@@ -2,53 +2,56 @@
     <section id="listing-popup-view" :key="'listing-view-' + sale.id ">
         <article class="listing-wrap">
 
-            <a @click="$router.go(-1)" class="cursor-pointer"><i class="far fa-map"></i> Back to map</a>
+            <router-link :to="{ name: 'listings'}" class="close-listing"><i class="fas fa-times"></i></router-link>
+<!--            <a @click="backToMap" class="close-listing"><i class="fas fa-times"></i></a>-->
 
-            <section v-if="loading" class="listing__loading">
-                Loading...
-            </section>
-
-            <div v-if="sale" class="container">
-                <section class="listing">
-                    <div class="row">
-                        <section class="col-12 col-md-7 col-lg-8">
-                            <div class="listing__main-content">
-                                <listing-images :images="sale.image"></listing-images>
-
-                                <header>
-                                    <h4>{{ sale.title }}</h4>
-                                </header>
-
-                                <section class="listing__actions">
-                                    <ul class="list-inline">
-                                        <li class="list-inline-item">
-                                            <a class="cursor-pointer" v-if="isSaved(sale.isSaved)"
-                                               @click="remove_saved_listing(sale)">
-                                                <i class="fas fa-star"></i> Saved
-                                            </a>
-                                            <a class="cursor-pointer" v-else @click="save_listing(sale)">
-                                                <i class="far fa-star"> Save</i>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </section>
-
-                                <section class="listing__description">
-                                    {{ sale.description }}
-                                </section>
-                            </div>
-                        </section>
-
-                        <aside class="col-12 col-md-5 col-lg-4">
-                            <p>{{ sale.address }}</p>
-                            <listing-dates :dates="sale.active_date"></listing-dates>
-                        </aside>
-                    </div>
+            <div class="single-listing__content">
+                <section v-if="loading" class="listing__loading">
+                    Loading...
                 </section>
+
+                <div v-if="sale" class="container">
+                    <section class="listing">
+                        <div class="row">
+                            <section class="col-12 col-md-7 col-lg-8">
+                                <div class="listing__main-content">
+                                    <listing-images :images="sale.image"></listing-images>
+
+                                    <header>
+                                        <h4>{{ sale.title }}</h4>
+                                    </header>
+
+                                    <section class="listing__actions">
+                                        <ul class="list-inline">
+                                            <li class="list-inline-item">
+                                                <a class="cursor-pointer" v-if="isSaved(sale.isSaved)"
+                                                   @click="remove_saved_listing(sale)">
+                                                    <i class="fas fa-star"></i> Saved
+                                                </a>
+                                                <a class="cursor-pointer" v-else @click="save_listing(sale)">
+                                                    <i class="far fa-star"> Save</i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </section>
+
+                                    <section class="listing__description">
+                                        {{ sale.description }}
+                                    </section>
+                                </div>
+                            </section>
+
+                            <aside class="col-12 col-md-5 col-lg-4">
+                                <p>{{ sale.address }}</p>
+                                <listing-dates :dates="sale.active_date"></listing-dates>
+                            </aside>
+                        </div>
+                    </section>
+                </div>
+
+                <RelatedListings :listing="sale"/>
+
             </div>
-
-            <RelatedListings :listing="sale"/>
-
         </article>
     </section>
 </template>
@@ -58,7 +61,6 @@
     import ListingImages from '../components/listings/ListingImages';
     import ListingDates from '../components/listings/ListingDates';
     import RelatedListings from '../components/RelatedListings';
-    import '../../sass/component/listings-popup-view.scss';
     import {listing_mixin} from '../components/listings/shared';
     import {isTrue} from '../helpers';
     import {SET_LISTING} from '../config/store/mutations';
@@ -101,6 +103,9 @@
             this.$store.commit(SET_LISTING, this.sale);
         },
         methods: {
+            backToMap() {
+                $router.go(-1);
+            },
             isSaved(val) {
                 return isTrue(val);
             },
@@ -147,3 +152,70 @@
     };
 
 </script>
+
+<style lang="scss">
+    @import '../../sass/colors';
+
+    .close-listing {
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding: 20px 20px;
+        color: white !important;
+        background-color: transparentize($tertiary, .4);
+        font-size: 28px;
+        transform: translateX(100%);
+    }
+
+    #listing-popup-view {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 100vh;
+        width: 100vw;
+        z-index: 9;
+
+        &::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            background-color: #000;
+            opacity: 0.5;
+            height: 100vh;
+            width: 100vw;
+        }
+
+        .listing-wrap {
+            position: absolute;
+            top: 80px;
+            left: 50%;
+            background-color: white;
+            box-shadow: 0 0 13px -6px;
+            width: 90vw;
+            height: 80vh;
+            max-width: 950px;
+            transform: translateX(-50%);
+        }
+
+        .single-listing__content {
+            padding: 30px;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+        }
+
+        .listing__main-content {
+            overflow: auto;
+        }
+
+        .listing {
+        }
+
+        .listing__dates {
+            max-width: 500px;
+        }
+
+    }
+
+</style>
